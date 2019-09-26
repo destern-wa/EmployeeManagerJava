2de85fde952609451f9537eb3ee0d238cbe0fe5a
David <20023188@tafe.wa.edu.au>
2019-09-26 10:48:06 +0800

    Added constructor and methods to Manager class

--- src/Manager.java
+++ src/Manager.java
@@ -13,4 +13,41 @@ import java.util.ArrayList;
  */
 public class Manager extends Employee {
     private ArrayList<Employee> employees;
+    
+    // Constructors
+    public Manager(String name, int number) {
+        super(name, number);
+    }
+    public Manager(String name, int number, ArrayList<Employee> employees) {
+        super(name, number);
+        this.employees = employees;
+    }    
+    
+    // Getters
+    public ArrayList<Employee> getEmployess() {
+        return employees;
+    }
+    
+    // Setters
+    public void addEmployee(Employee newEmployee) {
+        if (newEmployee != null) {
+            employees.add(newEmployee);
+        }
+    }
+    public void removeEmployee(Employee newEmployee) {
+        employees.remove(newEmployee);
+    }
+    
+    @Override
+    public String printDetails() {
+        String managerDetails = "Manager " + name + " / " + number + "/n";
+        if (employees.isEmpty()) {
+            return managerDetails + "(no employess)";
+        }
+        managerDetails += "Employees:";
+        for (Employee employee : employees) {
+            managerDetails += "\n- " + employee.printDetails();
+        }
+        return managerDetails;
+    }
 }
