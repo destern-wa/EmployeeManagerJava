5e32e0e8b97b0de8e8cf9ef0df298bc9319895a4
David <20023188@tafe.wa.edu.au>
2019-09-26 10:29:48 +0800

    Added constructor and methods to Employee class

--- src/Employee.java
+++ src/Employee.java
@@ -10,5 +10,35 @@
  */
 public class Employee {
     protected String name;
-    protected int number;    
+    protected int number;
+    
+    // Constructor
+    public Employee(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+    
+    // Getters
+    public String getName() {
+        return name;
+    }
+    public int getNumber() {
+        return number;
+    }
+    
+    // Setters
+    public void setName(String name) {
+        if (name != null && name.isEmpty()) {
+            this.name = name;
+        }
+    }
+    public void setNumber(int number) {
+        if (number > 0) {
+            this.number = number;
+        }
+    }
+    
+    public String printDetails() {
+        return name + " / " + number;
+    }
 }
